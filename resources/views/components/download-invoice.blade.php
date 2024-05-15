acb3a26c99a798c7a1aeee5d2985512d9ca247e3
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-15 19:34:30 +0600

    Fix

--- resources/views/components/download-invoice.blade.php
+++ resources/views/components/download-invoice.blade.php
@@ -1,32 +1,88 @@
 <!doctype html>
 <html>
 <head>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        body {
+            font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+        }
+
+        .text-gray-600 {
+            color: #4b5563;
+        }
+
+        .uppercase {
+            text-transform: uppercase;
+        }
+
+        .font-size-14 {
+            font-size: 14px;
+        }
+
+        .font-size-12 {
+            font-size: 12px;
+        }
+
+        .font-size-10 {
+            font-size: 10px;
+        }
+
+        .text-red-900 {
+            color: #991b1b;
+        }
+
+        .font-bold {
+            font-weight: bold;
+        }
+
+        .flex-wrap {
+            flex-wrap: wrap;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        td {
+            border: 1px solid black;
+            vertical-align: middle; /* Vertically center content */
+        }
+
+    </style>
+    <title>Invoice Receipt {{date('m/d/Y h:i:s a', time())}}</title>
 </head>
 <body>
 @if(!empty($packingReceipts))
-    <div class="grid grid-cols-2 gap-2 p-2">
+    <div>
+        <h4 style="margin-bottom: 4px; font-weight: bold; text-align: center">Invoice
+            Receipt {{date('m/d/Y h:i:s a', time())}}</h4>
         @foreach($packingReceipts as $receipt)
-            <div class="border-2 border-black border-solid rounded-md p-3">
-                <div class="flex justify-between">
-                    <div class="flex-1 overflow-hidden">
-                        <ul>
-                            <li class="text-gray-600 uppercase" style="font-size:14px">Customer</li>
-                            <li style='font-size:12px flex-wrap'>{{ $receipt['name'] }}</li>
-                            <li style='font-size:10px'>{{ $receipt['phone_number'] }}</li>
-                            <li class="flex-wrap" style='font-size:10px'>{{ $receipt['address'] }}</li>
-                        </ul>
-                    </div>
-                    <div class="flex-1 overflow-hidden">
-                        <ul>
-                            <li class="text-gray-600 uppercase" style="font-size:14px">Order # {{ $receipt['id'] }}</li>
-                            <li style='font-size:12px'>Method: {{ $receipt['shipping_provider_name'] }}</li>
-                            <li style='font-size:12px' class='font-bold'>Consgt: {{ $receipt['shipping_id'] }}</li>
-                            <li style='font-size:12px' class="text-red-900">Due: {{ $receipt['due_amount'] }} BDT</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            <table style="border-style: solid;margin-top:8px;margin-bottom: 8px; width: 100%;">
+                <tr>
+                    <td style="width: 50%">
+                        <p style="padding-left: 8px">
+                            <span class="text-gray-600 uppercase font-size-14">Customer</span>
+                            <br>
+                            <span class="flex-wrap font-size-12">{{ $receipt['name'] }}</span>
+                            <br>
+                            <span class="font-size-10">{{ $receipt['phone_number'] }}</span>
+                            <br>
+                            <span class="flex-wrap font-size-10">{{ $receipt['address'] }}</span>
+                        </p>
+                    </td>
+                    <td style="width: 50%">
+                        <p style="padding-left: 8px">
+                            <span class="text-gray-600 uppercase font-size-14">Order #{{ $receipt['id'] }}</span>
+                            <br>
+                            <span class="font-size-12">Method: {{ $receipt['shipping_provider_name'] }}</span>
+                            <br>
+                            <span class='font-bold font-size-12'>Consgt: {{ $receipt['shipping_id'] }}</span>
+                            <br>
+                            <span class="text-red-900 font-size-12">Due: {{ $receipt['due_amount'] }} BDT</span>
+                        </p>
+                    </td>
+                </tr>
+            </table>
         @endforeach
     </div>
 @endif
