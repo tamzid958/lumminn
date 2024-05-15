ff17a80466bd7b9e60b6f5f0e4b8cec8430e4eac
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-15 14:17:41 +0600

    Add deploy and fix product card

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Panel;
 
 
 class User extends Authenticatable implements MustVerifyEmail, FilamentUser
@@ -36,6 +36,11 @@ class User extends Authenticatable implements MustVerifyEmail, FilamentUser
         'remember_token',
     ];
 
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@lumminn.com') && $this->hasVerifiedEmail();
+    }
+
     /**
      * Get the attributes that should be cast.
      *
@@ -47,10 +52,5 @@ class User extends Authenticatable implements MustVerifyEmail, FilamentUser
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function canAccessPanel(Panel $panel): bool
-    {
-        return str_ends_with($this->email, '@lumminn.com');
     }
 }
