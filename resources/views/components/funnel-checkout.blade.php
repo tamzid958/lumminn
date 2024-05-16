146c7579e56127f846dd9a660cc3afbf10eb6988
Tamzid Ahmed <tamjidahmed958@gmail.com>
2024-05-16 22:43:33 +0600

    Update funnel-checkout.blade.php

--- resources/views/components/funnel-checkout.blade.php
+++ resources/views/components/funnel-checkout.blade.php
@@ -50,8 +50,8 @@
                                     <div class="relative flex items-center">
                                         <button type="button" id="decrement-button"
                                                 data-input-counter-decrement="counter-input"
-                                                class="flex-shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
-                                            <svg class="w-2.5 h-2.5 text-gray-900 dark:text-white" aria-hidden="true"
+                                                class="flex-shrink-0 bg-gray-700 hover:bg-gray-600 border-gray-600 inline-flex items-center justify-center border rounded-md h-5 w-5 focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                                            <svg class="w-2.5 h-2.5 text-white" aria-hidden="true"
                                                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                                                 <path stroke="currentColor" stroke-linecap="round"
                                                       stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
@@ -63,8 +63,8 @@
                                                placeholder="" value="1" required/>
                                         <button type="button" id="increment-button"
                                                 data-input-counter-increment="counter-input"
-                                                class="flex-shrink-0 bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 inline-flex items-center justify-center border border-gray-300 rounded-md h-5 w-5 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
-                                            <svg class="w-2.5 h-2.5 text-gray-900 dark:text-white" aria-hidden="true"
+                                                class="flex-shrink-0 bg-gray-700 hover:bg-gray-600 border-gray-600 inline-flex items-center justify-center border rounded-md h-5 w-5 focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                                            <svg class="w-2.5 h-2.5 text-white" aria-hidden="true"
                                                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                                                 <path stroke="currentColor" stroke-linecap="round"
                                                       stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
