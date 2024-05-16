5622cf64c726df4049125b03ebbdca0d19dd0c90
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-17 02:29:55 +0600

    Add files

---
+++ app/Filament/Widgets/Metrics.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\Expense;
+use App\Utils\NumberUtil;
+use App\Models\Investment;
+use App\Models\Order;
+
+class Metrics extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $expenses = Expense::selectRaw('EXTRACT(YEAR FROM expense_date) as year, EXTRACT(MONTH FROM expense_date) as month, SUM(amount) as total')
+        ->groupBy('year', 'month')
+        ->get();
+
+        $expenseArray = $expenses->pluck('total')->toArray();
+        $totalExpense = array_sum($expenseArray);
+
+        $investments = Investment::selectRaw('EXTRACT(YEAR FROM investment_date) as year, EXTRACT(MONTH FROM investment_date) as month, SUM(amount) as total')
+        ->groupBy('year', 'month')
+        ->get();
+
+        $invesmentArray = $investments->pluck('total')->toArray();
+        $totalInvestment = array_sum($invesmentArray);
+
+        $revenues = Order::selectRaw('EXTRACT(YEAR FROM created_at) as year, EXTRACT(MONTH FROM created_at) as month, SUM(total_amount + additional_amount - discount_amount) as total')
+        ->where('pay_status', '=', 'Paid')
+        ->groupBy('year', 'month')
+        ->get();
+
+        $revenueArray = $revenues->pluck('total')->toArray();
+        $totalRevenue = array_sum($revenueArray);
+
+        return [
+            Stat::make('Total Investment', NumberUtil::number_shorten($totalInvestment))
+            ->description('Increment of Investment')
+            ->descriptionIcon('heroicon-o-arrow-trending-up')
+            ->chart($invesmentArray)
+            ->extraAttributes(['title' => '৳'. $totalInvestment])
+            ->color('info'),
+
+            Stat::make('Total Expense', NumberUtil::number_shorten($totalExpense))
+            ->chart($expenseArray)
+            ->description('Decrement of Investment')
+            ->descriptionIcon('heroicon-o-arrow-trending-down')
+            ->extraAttributes(['title' => '৳'. $totalExpense])
+            ->color('danger'),
+
+            Stat::make('Total Revenue', NumberUtil::number_shorten($totalRevenue))
+            ->description('Increment of Revenue')
+            ->descriptionIcon('heroicon-o-arrow-trending-up')
+            ->chart($revenueArray)
+            ->extraAttributes(['title' => '৳'. $totalRevenue])
+            ->color('success'),
+        ];
+    }
+}
