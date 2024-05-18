deb0861b22ca9d952ea3dddb45100711bc1cccae
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-18 17:23:14 +0600

    Fix

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,15 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware([('lscache:max-age=31536000;public;esi=on')])->group(function () {
-    Route::get('/', [IndexController::class, 'index'])->name('home');
     Route::get('/categories/{slug}', [CategoryController::class, 'products']);
-    
-    Route::get('/products/{slug}', [ProductController::class, 'view']);
-    Route::post('/product/calculate', [ProductController::class, 'calculate']);
-
-    Route::post('/order/create', [OrderController::class, 'create'])->withoutMiddleware([VerifyCsrfToken::class]);
 });
+
+Route::get('/', [IndexController::class, 'index'])->name('home');
+   
+Route::get('/products/{slug}', [ProductController::class, 'view']);
+Route::post('/product/calculate', [ProductController::class, 'calculate']);
+
+Route::post('/order/create', [OrderController::class, 'create']);
 
 Route::any('/order/success/{invoice_id}', [OrderController::class, 'success'])->withoutMiddleware([VerifyCsrfToken::class]);
 Route::any('/order/fail-or-cancel/{invoice_id}', [OrderController::class, 'failOrCancel'])->withoutMiddleware([VerifyCsrfToken::class]);
