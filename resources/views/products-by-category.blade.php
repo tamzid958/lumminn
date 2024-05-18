fc3444a05da6cfbd1445e8674420fcf8038716a6
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-19 03:30:45 +0600

    Fix margin

--- resources/views/products-by-category.blade.php
+++ resources/views/products-by-category.blade.php
@@ -1,7 +1,7 @@
 @extends('components.layouts.app', ['title' => $category->name])
 
 @section('content')
-    <div class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:gap-4 sm:gap-2 gap-4 md:px-0 px-2 max-w-7xl mx-auto">
+    <div class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:gap-4 sm:gap-2 gap-4 md:px-0 px-2 max-w-7xl mx-auto mb-5">
         @foreach ($products as $product)
             <x-product-card :product='$product' />
         @endforeach
