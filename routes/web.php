e4370ba8552a7ecfa1a494a1c45a02248a40d625
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-18 17:11:20 +0600

    Fix

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@ Route::middleware([('lscache:max-age=31536000;public;esi=on')])->group(function 
     Route::get('/', [IndexController::class, 'index'])->name('home');
     Route::get('/categories/{slug}', [CategoryController::class, 'products']);
     Route::get('/products/{slug}', [ProductController::class, 'view']);
-    Route::post('/order/create', [OrderController::class, 'create']);
     Route::post('/product/calculate', [ProductController::class, 'calculate']);
 });
 
+Route::post('/order/create', [OrderController::class, 'create']);
 Route::any('/order/success/{invoice_id}', [OrderController::class, 'success'])->withoutMiddleware([VerifyCsrfToken::class]);
 Route::any('/order/fail-or-cancel/{invoice_id}', [OrderController::class, 'failOrCancel'])->withoutMiddleware([VerifyCsrfToken::class]);
 
