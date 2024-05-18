eca15ef5f19030d2ee7be4b3bf1d2ad8c4d78136
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-18 16:52:20 +0600

    Fix cache

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,13 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, 'index'])->name('home');
-
 Route::middleware([('lscache:max-age=31536000;public;esi=on')])->group(function () {
+    Route::get('/', [IndexController::class, 'index'])->name('home');
     Route::get('/categories/{slug}', [CategoryController::class, 'products']);
     Route::get('/products/{slug}', [ProductController::class, 'view']);
     Route::post('/order/create', [OrderController::class, 'create']);
+    Route::post('/product/calculate', [ProductController::class, 'calculate']);
 });
-
-Route::post('/product/calculate', [ProductController::class, 'calculate']);
 
 Route::any('/order/success/{invoice_id}', [OrderController::class, 'success'])->withoutMiddleware([VerifyCsrfToken::class]);
 Route::any('/order/fail-or-cancel/{invoice_id}', [OrderController::class, 'failOrCancel'])->withoutMiddleware([VerifyCsrfToken::class]);
