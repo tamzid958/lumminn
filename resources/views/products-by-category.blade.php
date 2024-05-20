446fcab4510a1ee70a742cce09835bb5ea3270f1
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-20 14:59:49 +0600

    Fix issue

--- resources/views/products-by-category.blade.php
+++ resources/views/products-by-category.blade.php
@@ -1,7 +1,7 @@
 @extends('components.layouts.app', ['title' => $category->name])
 
 @section('content')
-    <div class="flex items-center justify-center h-screen">
+    <div class="flex flex-col items-center justify-center h-screen">
         <div
             class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:gap-4 sm:gap-2 gap-4 md:px-0 px-2 max-w-7xl mb-5">
             @foreach ($products as $product)
