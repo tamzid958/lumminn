94e831022ff3f1376a4fc05e0e0a6b3567fd7342
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-20 14:58:51 +0600

    Fix list

--- resources/views/products-by-category.blade.php
+++ resources/views/products-by-category.blade.php
@@ -1,12 +1,13 @@
 @extends('components.layouts.app', ['title' => $category->name])
 
 @section('content')
-    <div
-        class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:gap-4 sm:gap-2 gap-4 md:px-0 px-2 max-w-7xl m-auto mb-5">
-        @foreach ($products as $product)
-            <x-product-card :product='$product'/>
-        @endforeach
+    <div class="flex items-center justify-center h-screen">
+        <div
+            class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:gap-4 sm:gap-2 gap-4 md:px-0 px-2 max-w-7xl mb-5">
+            @foreach ($products as $product)
+                <x-product-card :product='$product'/>
+            @endforeach
+        </div>
+        {{ $products->links() }}
     </div>
-
-    {{ $products->links() }}
 @endsection
