f7ebf9c42640a1462106219d7ad4c58b44d6dadd
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-21 20:39:23 +0600

    Remove time

--- routes/console.php
+++ routes/console.php
@@ -11,7 +11,6 @@ Artisan::command('inspire', function () {
 
 Schedule::job(new CheckDeliveryStatusJob)
     ->everyThirtyMinutes()
-    ->between('7:00', '22:00')
     ->before(fn () => dump('CheckDeliveryStatusJob Started'))
     ->after(fn () => dump('CheckDeliveryStatusJob Finished'))
     ->withoutOverlapping();
