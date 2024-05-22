89aa9bc4130864e86373b34afacb8c39c7c19f91
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-22 22:27:46 +0600

    Fix products

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -12,7 +13,13 @@ class CategoryController extends Controller
     {
 
         $category = Category::where('slug', $slug)->first();
-        $products = Product::query()->where('category_id', $category->id)->orderBy('created_at', "desc")->cursorPaginate(6);
+        $products = Product::query()
+                    ->select('products.*', DB::raw('SUM(order_items.quantity) as total_sold'))
+                    ->join('order_items', 'order_items.product_id', '=', 'products.id')
+                    ->where('category_id', $category->id)->orderBy('created_at', "desc")
+                    ->groupBy('products.id')
+                    ->orderBy('total_sold', 'desc')
+                    ->cursorPaginate(6);
         return view('products-by-category', compact('products', 'category'));
     }
 }
