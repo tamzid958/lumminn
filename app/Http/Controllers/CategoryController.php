1fe078593f3aef9bff81dba2c4b2816c7b5b71bc
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-22 22:39:28 +0600

    Fix query

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,12 +14,12 @@ class CategoryController extends Controller
 
         $category = Category::where('slug', $slug)->first();
         $products = Product::query()
-                    ->select('products.*', DB::raw('COALESCE(SUM(order_items.quantity), 0) as total_sold'))
-                    ->leftJoin('order_items', 'order_items.product_id', '=', 'products.id')
-                    ->where('category_id', $category->id)
-                    ->groupBy('products.id')
-                    ->orderBy('total_sold', 'desc')
-                    ->cursorPaginate(6);
+        ->select('products.*', DB::raw('COALESCE(SUM(order_items.quantity), 0) as total_sold'))
+        ->leftJoin('order_items', 'order_items.product_id', '=', 'products.id')
+        ->where('products.category_id', $category->id)
+        ->groupBy('products.id')
+        ->orderBy('total_sold', 'desc')
+        ->cursorPaginate(6);
         return view('products-by-category', compact('products', 'category'));
     }
 }
