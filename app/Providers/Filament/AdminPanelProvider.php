5719d22f2e441b21c9eedd897a97f66a23bf1c0d
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-22 23:56:03 +0600

    Fix

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -38,6 +38,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->profile()
+            ->emailVerification()
             ->colors([
                 'primary' => Color::Indigo,
             ])
