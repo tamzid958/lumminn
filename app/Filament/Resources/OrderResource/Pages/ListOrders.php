dec84242d63e48e00e9659cc4d50d174db476c54
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-24 05:07:44 +0600

    Fix

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Jobs\CheckDeliveryStatusJob;
 use Filament\Actions;
 use Filament\Actions\Action;
 use Filament\Resources\Pages\ListRecords;
@@ -15,11 +16,19 @@ class ListOrders extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            Actions\ActionGroup::make([
             Action::make('send')
                 ->label('Send Orders')
                 ->icon('heroicon-o-paper-airplane')
                 ->color('success')
                 ->url(OrderResource::getUrl('send')),
+            Action::make('sync')
+                ->label('Sync Orders')
+                ->icon('heroicon-o-arrow-path')
+                ->color('primary')
+                ->action(fn() => dispatch(new CheckDeliveryStatusJob())),
+            ],
+            )->label('Manage Orders')->button()->color('success')
         ];
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -11,8 +11,6 @@ Artisan::command('inspire', function () {
 
 Schedule::job(new CheckDeliveryStatusJob)
     ->everyThreeHours()
-    ->timezone('Asia/Dhaka')
-    ->between('7:00', '23:00')
     ->before(fn () => dump('CheckDeliveryStatusJob Started'))
     ->after(fn () => dump('CheckDeliveryStatusJob Finished'))
     ->withoutOverlapping();
