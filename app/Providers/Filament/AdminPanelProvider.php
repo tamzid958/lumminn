12a457180a545514ced54c6207cdaf8da31c00e4
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-24 11:10:34 +0600

    Add email verification

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,6 +37,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->emailVerification()
             ->profile(isSimple: false)
             ->colors([
                 'primary' => Color::Indigo,
