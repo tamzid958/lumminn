073b05bc2f50ff3233c3caa8bfec1bed89a4eec5
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2024-05-28 00:01:44 +0600

    Fix

--- app/Filament/Resources/OrderResource/Widgets/TotalOrderCount.php
+++ app/Filament/Resources/OrderResource/Widgets/TotalOrderCount.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\OrderResource\Widgets;
 
+use App\Models\Enum\ShippingStatus;
 use App\Models\Order;
 use Filament\Forms\Components\Select;
 use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
@@ -70,26 +71,39 @@ class TotalOrderCount extends ApexChartWidget
         $currentMonth = $this->filterFormData['month'];
 
         $numDaysInMonth = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
+        $enumValues = array_column(ShippingStatus::cases(), 'value');
 
-        // Loop through each day of the current month
+        $totalOrderCounts = array_fill(1, $numDaysInMonth, 0);
+
+        // Initialize an array to hold the order counts for each shipping status and day
+        $orderCounts = array_fill_keys($enumValues, array_fill(1, $numDaysInMonth, 0));
+
+        // Loop through each day of the current month and gather order counts
         for ($day = 1; $day <= $numDaysInMonth; $day++) {
             // Format the date for the current day
-            $currentDate = date('Y-m-d', mktime(0, 0, 0, $currentMonth, $day, $currentYear));
+            $currentDate = sprintf('%04d-%02d-%02d', $currentYear, $currentMonth, $day);
+            $startDateTime = "$currentDate 00:00:00";
+            $endDateTime = "$currentDate 23:59:59";
 
-            // Get the start and end datetime for the current day
-            $startDateTime = $currentDate . ' 00:00:00';
-            $endDateTime = $currentDate . ' 23:59:59';
+            $totalOrderCount = Order::query()
+                            ->whereBetween('created_at', [$startDateTime, $endDateTime])
+                            ->count();
+            $totalOrderCounts[$day] = $totalOrderCount;
 
-            // Get the order count for the current day
-            $orderCount = Order::query()
+            // Query orders created on the current day, grouped by shipping status
+            $dailyOrderCounts = Order::query()
                 ->whereBetween('created_at', [$startDateTime, $endDateTime])
-                ->count();
+                ->selectRaw('shipping_status, COUNT(*) as count')
+                ->groupBy('shipping_status')
+                ->pluck('count', 'shipping_status')
+                ->toArray();
 
-            // Store the order count in the array with the day as the key
-            $orderCounts[date('d', mktime(0, 0, 0, $currentMonth, $day, $currentYear))] = $orderCount;
+            // Store the counts in the orderCounts array
+            foreach ($dailyOrderCounts as $status => $count) {
+                $orderCounts[$status][$day] = $count;
+            }
         }
-
-
+        
         return [
             'chart' => [
                 'type' => 'area',
@@ -97,13 +111,21 @@ class TotalOrderCount extends ApexChartWidget
             ],
             'series' => [
                 [
-                    'tickAmount' => "dataPoints",
-                    'name' => 'Order Count',
-                    'data' => array_values($orderCounts),
+                    'tickAmount' => 'dataPoints',
+                    'name' => 'Total Orders',
+                    'data' => array_values($totalOrderCounts),
                 ],
+                ...array_map(function ($status) use ($orderCounts) {
+                    return [
+                        'tickAmount' => 'dataPoints',
+                        'name' => $status,
+                        'data' => array_values($orderCounts[$status]),
+                    ];
+                }, $enumValues)
             ],
             'xaxis' => [
-                'categories' => array_keys($orderCounts),
+                'type'=> 'date',
+                'categories' => range(1, $numDaysInMonth),
                 'labels' => [
                     'show'=> false,
                     'style' => [
@@ -121,7 +143,7 @@ class TotalOrderCount extends ApexChartWidget
                     ],
                 ],
             ],
-            'colors' => ['#059669'],
+            'colors' => ['#ffffff', '#6e767d', '#5c80bc', '#d88432', '#3fa372', '#d84c4c', '#85329c'],
             'stroke' => [
                 'curve' => 'smooth',
             ],
